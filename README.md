baffc244edc9531ee4bb9ec2d03ec6ca024bf676
Coby Tamayo <acobster@gmail.com>
2021-12-28 21:59:52 -0700

    start hacking on a chatbot

---
+++ README.md
@@ -0,0 +1,21 @@
+# Artur
+
+A chatbot for initiating torrent downloads. Uses [Twilio](https://www.twilio.com/docs/sms) for sending and receiving SMS messages, and [rTorrent](https://rtorrent-docs.readthedocs.io/en/latest/) for downloading.
+
+## Requirements
+
+* rTorrent, installed on your `PATH`
+* Twilio account, with some webhook setup required
+* A purchased Twilio number, for receiving texts
+* A recent version of Java
+
+## How it works
+
+1. You send a torrent URL to your Twilio number
+2. Twilio forwards it to your configured webhook (one you've set up in advance)
+3. The app sends a response asking which folder to download the files to (configured via env vars)
+4. You respond with something like "movies" (a key in your download folders env var)
+5. The download begins
+6. The app sends updates every $x minutes (another env var) as the download progresses ??? and a final update once it has finished
+
+Steps 3-6 are driven by a finite state machine that keeps track of where the conversation is and what to do next.
---
+++ src/artur/app.clj
@@ -0,0 +1,44 @@
+(ns artur.app
+  (:gen-class)
+  (:require
+    [config.core :as config]
+    [mount.core :as mount :refer [defstate]]
+    [org.httpkit.server :as http]
+    [reitit.core :as reitit]
+    [ring.middleware.defaults :as ring]))
+
+(defstate env
+  :start (config/load-env))
+
+(defn handler [req]
+  (prn req)
+  {:status 200
+   :headers {"content-type" "application/xml"}
+   :body "oh hai"})
+
+(def app
+  (ring/wrap-defaults
+    handler
+    (assoc ring/api-defaults
+           :cookies false
+           :params {:keywordize true
+                    :urlencoded true}
+           ;; TODO https://github.com/ring-clojure/ring-defaults#customizing
+           )))
+
+(defonce stop-server (atom nil))
+(defstate http-server
+  :start (reset! stop-server (http/run-server
+                               #'app
+                               {:port (:port env 3000)}))
+  :stop (when (fn? @stop-server) (@stop-server)))
+
+(defn -main [& _]
+  (mount/start))
+
+(comment
+  (do
+    (mount/stop)
+    (mount/start))
+
+  (slurp "http://localhost:3003"))
