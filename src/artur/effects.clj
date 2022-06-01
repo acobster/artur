70d4f75a3e5ff6f6e30509d57653ecba24007f72
Coby Tamayo <coby@tamayo.email>
2022-06-01 00:20:38 -0700

    More robust torrent file downloads

--- src/artur/effects.clj
+++ src/artur/effects.clj
@@ -1,11 +1,23 @@
-(ns artur.effects)
+(ns artur.effects
+  (:require
+    [artur.config :refer [env]]))
 
 (defmulti effect! first)
 
 (defmethod effect! :download [[_ download]]
   (let [url (:url download)
-        file (last (clojure.string/split url #"/"))]
-    (spit file (slurp url))))
+        file (last (clojure.string/split url #"/"))
+        dir (:download-dir env "./downloads")
+        file-path (str dir java.io.File/separator file)
+        torrent (try
+                  (slurp url)
+                  (catch java.io.FileNotFoundException e
+                    (println "Error downloading torrent file:" e)))]
+    (when torrent
+      (when (.mkdir (java.io.File. dir))
+        (println "created directory " dir))
+      (println (format "downloading torrent at %s to %s (%d B)" url file-path (count torrent)))
+      (spit file-path torrent))))
 
 (defn wrap-effects [handler]
   (fn [req]
