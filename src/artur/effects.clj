f27133cc04209e5a46b05ac377086920ea52d5e9
Coby Tamayo <coby@tamayo.email>
2022-05-31 23:50:35 -0700

    implement simplistic downloads

--- src/artur/effects.clj
+++ src/artur/effects.clj
@@ -2,9 +2,18 @@
 
 (defmulti effect! first)
 
+(defmethod effect! :download [[_ download]]
+  (let [url (:url download)
+        file (last (clojure.string/split url #"/"))]
+    (spit file (slurp url))))
+
 (defn wrap-effects [handler]
   (fn [req]
     (let [res (handler req)]
       (doseq [effect (:effects res)]
-        (effect! effect))
+        (try
+          (effect! effect)
+          (catch Throwable e
+            ;; TODO actual error handling
+            (prn 'ERROR e))))
       res)))
